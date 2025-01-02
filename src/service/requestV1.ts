25b96efd4c30742c46389b63465b16c25545d765
zhaoguoqiang <879898082@qq.com>
2025-01-02 16:16:17 +0800

    完善requestV1

--- src/service/requestV1.ts
+++ src/service/requestV1.ts
@@ -1,9 +1,11 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosRequestHeaders } from 'axios';
+
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.1.52:5000';
 
 // 创建 Axios 实例
 const Axios: AxiosInstance = axios.create({
-    baseURL: 'https://api.example.com', // 替换为你的 API 基础 URL
-    timeout: 10000, // 设置请求超时时间
+    baseURL,
+    timeout: 30000, // 设置请求超时时间
 });
 
 // 请求拦截器
@@ -13,6 +15,11 @@ Axios.interceptors.request.use(
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
+
+        if (!config.headers['Content-Type']) {
+            config.headers['Content-Type'] = 'application/json';
+        }
+
         return config;
     },
     error => {
@@ -41,16 +48,30 @@ Axios.interceptors.response.use(
     }
 );
 
-export default function ({ url, method, data, headers }: InternalAxiosRequestConfig) {
+export default function <T>({
+    url,
+    method,
+    data,
+    headers = {} as AxiosRequestHeaders,
+}: Partial<InternalAxiosRequestConfig>): Promise<AxiosResponse<T>> {
     const options: InternalAxiosRequestConfig = {
         url,
         method,
-        headers
+        headers,
     };
     if (method && method.toLowerCase() === 'get') {
         options.params = data;
     } else {
         options.data = data;
     }
-    return Axios(options);
+
+    return new Promise((resolve, reject) => {
+        Axios(options)
+            .then(response => {
+                resolve(response);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
 }
